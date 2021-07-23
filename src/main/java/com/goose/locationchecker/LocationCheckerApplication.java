94dc9ca237295e13176bc9133a4951b7e2571231
Oleksandr Korchack <sanchoskorch765@gmail.com>
2021-07-23 17:06:43 +0300

    first commit

---
+++ src/main/java/com/goose/locationchecker/LocationCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.goose.locationchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationCheckerApplication.class, args);
+    }
+
+}
